fe67b20b7bd210d0e0a919e628f11a1b43e20fc3
Hbb Moussa <habibou997@gmail.com>
2022-10-22 14:03:58 +0000

    authentication  Method

---
+++ JAVA/TryingMethods.java
@@ -0,0 +1,29 @@
+/*This Class is for understand the difference between :
+ * - Static and Dynamic method
+ * - Handle The control and loop statement 
+ * -
+ */
+
+public class TryingMethods {
+	
+	//Main method
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	/*	If you want to execute a method 
+	 * 	Just Call it inside the main method .
+	 * Like This 
+	 */
+		
+	}
+	
+	// Authentication
+	public void authentication(String userName, String password) {
+		if (userName == "Habibou" & password == "moussa123") {
+			System.out.println("Authentication Success");
+		}else if (userName == "Uche" & password == "uche123") {
+			System.out.println("Authentication Success");
+		}else System.out.println("Wrong Username or Password !");
+	}
+		
+
+}
