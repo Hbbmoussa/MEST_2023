0b41492f445d826cdd764b65a072e38969d1d1d4
Hbb Moussa <habibou997@gmail.com>
2022-10-22 13:46:28 +0000

    Creating Main Class

---
+++ JAVA/TryingMethods.java
@@ -0,0 +1,21 @@
+/*This Class is for understand the difference between :
+ * - Static and Dynamic method
+ * - Handle The control and loop statement 
+ * -
+ */
+
+public class TryingMethods {
+	
+	//Main method
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	/*	If you want to execute a method 
+	 * 	Just Call it inside the main method .
+	 * Like This 
+	 */
+		
+		
+	}
+		
+
+}
