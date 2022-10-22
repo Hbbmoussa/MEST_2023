d1f24518bc42011999bcbb89f825e7c83a2162b4
Hbb Moussa <habibou997@gmail.com>
2022-10-22 14:10:57 +0000

    even Numbers  Method

---
+++ JAVA/TryingMethods.java
@@ -0,0 +1,32 @@
+/*This Class is for understand the difference between :
+ * - Static and Dynamic method
+ * - Handle The control and loop statement 
+ * -
+ */
+
+public class TryingMethods {
+	
+	//Main method
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	/*	If you want to execute a method 
+	 * 	Just Call it inside the main method .
+	 * Like This 
+	 */
+		
+	}
+	
+	/// Even Numbers with for loop
+	public static void evenNumbers() {
+		int sumOfEven = 0;		
+		for (int i=12; i<103; i++) {
+			if( i%2 == 0) {
+				sumOfEven += i;
+			}else ;
+		}
+		
+		System.out.println(sumOfEven);
+	}
+		
+
+}
